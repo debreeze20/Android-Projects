54661c8afb922fbb725973e66faa70de62e347f6
Desi <83933926+debreeze20@users.noreply.github.com>
2024-04-02 14:07:17 +0700

    separated "UTS" to another repo

--- UTS/File/dhethrift/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $host = "localhost";
-    $user = "root";
-    $pass = "";
-    $db = "dbthrift";
-
-    $konek = mysqli_connect ($host, $user, $pass, $db) or die ("Database Mysql Tidak Terhubung");   
